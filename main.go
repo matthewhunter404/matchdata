0ff905e48d49295e09131c9ae2139973c5ed8e28
Matthew Hunter <matthewhunter404@gmail.com>
2022-09-25 10:41:02 +0200

    Store footballData in a struct rather than a .csv file
    Add parsing of csv data into a slice of structs
    Delete output.csv
    Add logging of data to the commandline in json format

--- main.go
+++ main.go
@@ -1,22 +1,29 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
+type FootballData struct {
+	div      string //League Division
+	date     string //Match Date (dd/mm/yy)
+	time     string //Time of match kick off
+	homeTeam string //Home Team
+	awayTeam string //Away Team
+	fthg     string //Full Time Home Team Goals
+	ftag     string //Full Time Away Team Goals
+}
+
 func FetchFile(url string) {
 
 	client := http.Client{
 		Timeout: 5 * time.Second,
 	}
-
-	out, err := os.Create("output.csv")
-	defer out.Close()
 
 	resp, err := client.Get(url)
 	if err != nil {
@@ -25,12 +32,30 @@ func FetchFile(url string) {
 	}
 	defer resp.Body.Close()
 
-	_, err = io.Copy(out, resp.Body)
+	csvReader := csv.NewReader(resp.Body)
+	records, err := csvReader.ReadAll()
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
-		return
 	}
 
+	var allData []FootballData
+	for _, record := range records {
+
+		footballData := FootballData{
+			div:      record[0],
+			date:     record[1],
+			time:     record[2],
+			homeTeam: record[3],
+			awayTeam: record[4],
+			fthg:     record[5],
+			ftag:     record[6],
+		}
+		allData = append(allData, footballData)
+	}
+
+	for _, match := range allData {
+		fmt.Printf("%+v\n", match)
+	}
 }
 
 func main() {
