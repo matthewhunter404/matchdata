7df003bb625d8f5512c8fb7a4cd8f375f08951be
Matthew Hunter <matthewhunter404@gmail.com>
2022-09-25 10:15:39 +0200

    Add fetch file function
    Add function that fetches the URL passed in the command line parameters and stores it in a .csv file

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func FetchFile(url string) {
+
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	out, err := os.Create("output.csv")
+	defer out.Close()
+
+	resp, err := client.Get(url)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return
+	}
+
+}
 
 func main() {
-	log.Println("Hello World")
+
+	url := os.Args[1]
+	log.Println("Running Program...")
+	log.Println(url)
+	FetchFile(url)
+
 }
